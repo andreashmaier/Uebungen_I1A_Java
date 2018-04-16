e9c4d9ad52a6e30b2d88551ed89eaefcd4557f1f
Andreas Maier <maier.gbs@sabel.com>
2018-04-16 12:56:34 +0200

    Testklasse added

---
+++ Technischer-Kundendienst/src/de/gbsschulen/technischerKundendienst/Testklasse.java
@@ -0,0 +1,9 @@
+package de.gbsschulen.technischerKundendienst;
+
+public class Testklasse {
+
+    public static void main(String[] args) {
+        Kundendienstsystem kundendienstsystem = new Kundendienstsystem();
+        kundendienstsystem.starten();
+    }
+}
